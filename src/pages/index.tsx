ce5dad9d2656694066c8c6a88f87eb513ab65e3e
B. P. Hon <baijiephoneycutt@gmail.com>
2023-11-28 12:37:53 -0500

    created project scaffold

---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { Typography } from "@mui/material";
+import { type ReactElement } from "react";
+
+export default function Home(): ReactElement {
+  return <Typography variant="h1"> HELLO WORLD </Typography>;
+}
